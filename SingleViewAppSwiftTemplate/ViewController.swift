b4a0143b6f1f7b4913c998e9899d357bf18e1798
Alan Longcoy <ajlongcoy21@gmail.com>
2017-02-12 12:00:14 -0600

    Added pictures for the up and down buttons pressed configuration. Also configured the buttons to switch pictures when they are pressed and released.

--- SingleViewAppSwiftTemplate/ViewController.swift
+++ SingleViewAppSwiftTemplate/ViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    @IBAction func moveEvent(_ sender: UIButton)
+    {
+        
+    }
+    
+    @IBAction func moveButtonPressedDown(_ sender: UIButton)
+    {
 
-    override func viewDidLoad() {
+    }
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
